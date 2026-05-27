d92b064d1a70d6e6be3483953d9feac695414c5c
Denny Septian Panggabean <xamidimura@gmail.com>
2026-05-27 11:41:10 +0700

    fix: config astro

--- docs-site/astro.config.mjs
+++ docs-site/astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import starlight from "@astrojs/starlight";
+import process from "node:process";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://phpdevsr.github.io',
-  base: process.env.ASTRO_BASE || '/js-validation',
-  trailingSlash: 'always',
-  outDir: '../_site',
+  site: "https://phpdevsr.github.io",
+  base: process.env.ASTRO_BASE || "/js-validation",
+  integrations: [
+    starlight({
+      title: "@phpdevsr/js-validation - A JavaScript validation library for Node.js and browsers.",
+    }),
+  ],
+  trailingSlash: "always",
+  outDir: "../_site",
 });
