8168fe3183c3e8ce914f6d1236e8853b01423741
Denny Septian Panggabean <xamidimura@gmail.com>
2026-05-27 18:17:49 +0700

    docs: make dynamic version

--- docs-site/eslint.config.js
+++ docs-site/eslint.config.js
@@ -1,17 +1,26 @@
 // @ts-check
-import js from '@eslint/js';
-import astro from 'eslint-plugin-astro';
+import js from "@eslint/js";
+import astro from "eslint-plugin-astro";
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
+  {
+    ignores: [
+      "**/.astro/**",
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/_site/**",
+    ],
+  },
+
   js.configs.recommended,
   ...astro.configs.recommended,
+
   {
     languageOptions: {
       globals: {
         __APP_VERSION__: "readonly",
       },
     },
-    ignores: ["node_modules/", "dist/", "../_site/", ".astro/"],
   },
 ];
